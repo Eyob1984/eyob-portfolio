f0ef87187a5b88d2c7846165f41af5ac3a1ee02b
Eyob Tamir <51892714+Eyob1984@users.noreply.github.com>
2023-07-04 08:26:05 -0700

    added some functionalty on textarea

--- src/Connect.js
+++ src/Connect.js
@@ -2,10 +2,19 @@ import React, { useState } from 'react';
 
 function Connect() {
   const [showForm, setShowForm] = useState(false);
+  const [char, setChar] = useState(250);
 
+  const style = {
+    width: '60%',
+    height: '10rem'
+  }
   const handleConnectClick = () => {
     setShowForm(true);
   };
+
+  const handleChar = () => {
+    setChar(char - 1)
+  }
 
   return (
     <div style={{border: '1px solid white', width: '50%', height: '60%', flex: '1', margin: '25px 0px 0px 15px'}}>
@@ -21,6 +30,7 @@ function Connect() {
                 className="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
+                style={{width: '60%'}}
               />
               <div id="emailHelp" className="form-text">
                 We'll never share your email with anyone else.
@@ -32,8 +42,11 @@ function Connect() {
                className='form-control'
                type='text '
                placeholder='Enter your text'
+               style={style}
+               onChange={handleChar}
                /> 
             </div>
+            <p>Maximum: {char}Char</p>
           </form>
         </div>
       )}
